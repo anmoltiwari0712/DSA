37f4431ff3d62246fbd8517b9c3a47a9d052332d
Anmol Tiwari <96834701+anmoltiwari0712@users.noreply.github.com>
2022-01-03 08:20:36 +0530

    Add files via upload

---
+++ areaofcircle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r;
+    int area;
+    cout<<"ENTER RADIUS OF CIRICLE"<<endl;
+    cin>>r;
+    cout<<"AREA OF CIRCLE IS "<<3.14*r*r<<endl;
+    cout<<"PERIMETER OF CIRCLE IS "<<2*3.14*r<<endl;
+
+    return 0;
+}
