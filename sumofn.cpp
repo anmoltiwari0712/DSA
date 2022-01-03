08968e424b7988ea782f476a6bf639b404790640
Anmol Tiwari <96834701+anmoltiwari0712@users.noreply.github.com>
2022-01-03 08:05:10 +0530

    Add files via upload

---
+++ sumofn.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    
+    cin>>n;
+
+    int sum=0;
+    for(int co=1;co<=n;co++){
+        sum=sum+co;
+    
+    }
+    cout<<sum<<endl;
+    return 0;
+}
