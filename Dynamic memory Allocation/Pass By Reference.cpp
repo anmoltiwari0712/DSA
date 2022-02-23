6e7183daaf0133a923f3c82ce9fc07ec1dcc68ff
Anmol Tiwari <96834701+anmoltiwari0712@users.noreply.github.com>
2022-02-23 15:40:53 +0530

    Add files via upload

---
+++ Dynamic memory Allocation/Pass By Reference.cpp
@@ -0,0 +1,23 @@
+//reference variable need
+#include<iostream>
+using namespace std;
+int update(int n){
+    n=n+1; //so here ek copy of n(new) create hota hai when function is called
+    //this n is only for the update function
+
+    
+}
+
+int main(){
+    
+    int n;
+    cin>>n;
+    update(n);
+    cout<<n<<endl;
+    //same value print hoga bcoz the n in function and n in cout both have diff memory locations
+    //* thats where reference variable is needed
+
+
+    
+
+}
