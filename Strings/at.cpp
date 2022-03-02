9509425882964fefa8c67f4fc78cfe5a7a381cba
Anmol Tiwari <96834701+anmoltiwari0712@users.noreply.github.com>
2022-03-02 20:23:08 +0530

    Add files via upload

---
+++ Strings/at.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    //indexing begins with 0
+    string str="anmol";
+    cout<<str.at(2)<<endl;
+    //or it can also be done just like arrays
+    //str[2];
+
+}
