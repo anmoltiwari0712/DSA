eeed8cb776425900a02610544aca52027f8f5383
Anmol Tiwari <96834701+anmoltiwari0712@users.noreply.github.com>
2022-03-02 20:21:39 +0530

    Add files via upload

---
+++ Strings/Append.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str;
+    string str1="fam";
+    string str2="ily";
+    str.append(str1);
+    str.append(str2);
+    cout<<str;
+
+
+    //or it can also be done using + operator
+    //str=str1+str2;
+
+
+}
+
